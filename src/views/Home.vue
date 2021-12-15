fc519c09772eb69cb05b3b1ab0d39245b7468345
kawabata <s1931115@s.konan-u.ac.jp>
2021-12-15 13:17:27 +0900

    add 一つ図した

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <Calendar />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import Calendar from "@/components/Calendar.vue";
 
 export default defineComponent({
   name: "Home",
   components: {
-    HelloWorld,
+    Calendar,
   },
 });
 </script>
