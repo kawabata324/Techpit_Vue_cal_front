247766b36ba3fb56030eb48a822a701650d2c344
kawabata <s1931115@s.konan-u.ac.jp>
2021-12-15 13:03:53 +0900

    first commit

---
+++ src/components/Calendar.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>Calendar</h1>
+    <p>events:</p>
+    <p>{{ events }}</p>
+    <button @click="fetchEvent">fetchEvent</button>
+  </div>
+</template>
+
+<script>
+import { defineComponent, reactive } from "@vue/runtime-core";
+import axios from "axios";
+
+export default defineComponent({
+  setup() {
+    let events = reactive({});
+    const fetchEvent = () => {
+      axios
+        .get("http://localhost:3011/events")
+        .then((res) => {
+          events = res.data;
+        })
+        .catch((e) => console.log(e));
+    };
+    return {
+      events,
+      fetchEvent,
+    };
+  },
+});
+</script>
+
+<style></style>
