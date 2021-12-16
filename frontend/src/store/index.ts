bbc9a3a5ce48a6d3ab07ceb83060b000a1c5fb1d
kawabata <s1931115@s.konan-u.ac.jp>
2021-12-16 21:28:55 +0900

    vuexを使用して値を管理する

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,10 +1,25 @@
 import { createStore } from "vuex";
+import axios from "axios";
+
+const apiUrl = "http://localhost:3011";
 
 export default createStore({
   state: {
-    
+    events: [],
   },
-  mutations: {},
-  actions: {},
+  getters: {
+    events: (state) => {
+      return state.events;
+    },
+  },
+  mutations: {
+    setEvents: (state, events) => (state.events = events),
+  },
+  actions: {
+    async fetchEvents({ commit }) {
+      const response = await axios.get(`${apiUrl}/events`);
+      commit("setEvents", response.data);
+    },
+  },
   modules: {},
 });
